cdcb72d84e8ac70483deada117a1ade1060767e4
akraievoy <akraievoy@gmail.com>
2012-02-21 13:24:20 +0200

    documented v03 algos

--- src/main/java/se/V03.java
+++ src/main/java/se/V03.java
@@ -1,35 +1,104 @@
 package se;
 
 public class V03 {
+    /**
+     * Алгоритм, який описує зміни ваги об’єктів у залежності від місця на орбіті
+     */
     public static interface WeightAlgo {
+        /**
+         * Обчислення ваги об’єкта в залежності від прогресу руху по орбіті
+         * та початкової ваги
+         *
+         * @param orbitProgression прогрес руху по орбіті,
+         *                         від 0 (початок) до 1(завершення)
+         * @param startWeight початкова вага
+         * @return обчислена вага
+         */
         double computeWeight(double orbitProgression, double startWeight);
     }
 
+    /**
+     * Алгоритм, що описує відсутність змін у вазі об’єкта
+     */
     public static class WeightAlgoConstant implements WeightAlgo {
+        /**
+         * Обчислення ваги об’єкта в залежності від прогресу руху по орбіті
+         * та початкової ваги
+         *
+         * @param orbitProgression прогрес руху по орбіті,
+         *                         від 0 (початок) до 1(завершення)
+         * @param startWeight початкова вага
+         * @return обчислена вага
+         */
         public double computeWeight(double orbitProgression, double startWeight) {
             return startWeight;
         }
     }
 
+    /**
+     * Алгоритм, що описує зміни у вазі об’єкта, що буде відділено
+     * на деякій фазі просування по орбіті
+     */
     public static class WeightAlgoDisposable implements WeightAlgo {
+        /**
+         * Фаза відділення об’єкта, від 0 до 1
+         */
         double disposeProgression;
 
+        /**
+         * Створення примірника алгоритма
+         *
+         * @param disposeProgression фаза відділення об’єкта, від 0 до 1
+         */
         public WeightAlgoDisposable(double disposeProgression) {
             this.disposeProgression = disposeProgression;
         }
 
+        /**
+         * Обчислення ваги об’єкта в залежності від прогресу руху по орбіті
+         * та початкової ваги
+         *
+         * @param orbitProgression прогрес руху по орбіті,
+         *                         від 0 (початок) до 1(завершення)
+         * @param startWeight початкова вага
+         * @return обчислена вага
+         */
         public double computeWeight(double orbitProgression, double startWeight) {
             return orbitProgression > disposeProgression ? 0 : startWeight;
         }
     }
 
+    /**
+     * Алгоритм, що описує зміни у вазі об’єкта,
+     * що починає постійно та рівномірно використовуватись
+     * на початку та повністю закінчується на заданому
+     * єтапі руху по орбіті
+     */
     public static class WeightAlgoExhaustable implements WeightAlgo {
+        /**
+         * Фаза орбіти (від 0 до 1), на якій об’єкт буде повністю використано
+         */
         double exhaustProgression;
 
+        /**
+         * Створення примірника алгоритма
+         *
+         * @param exhaustProgression фаза повного використання об’єкта,
+         *                           від 0 до 1
+         */
         public WeightAlgoExhaustable(double exhaustProgression) {
             this.exhaustProgression = exhaustProgression;
         }
 
+        /**
+         * Обчислення ваги об’єкта в залежності від прогресу руху по орбіті
+         * та початкової ваги
+         *
+         * @param orbitProgression прогрес руху по орбіті,
+         *                         від 0 (початок) до 1(завершення)
+         * @param startWeight початкова вага
+         * @return обчислена вага
+         */
         public double computeWeight(double orbitProgression, double startWeight) {
             final double exhaustRatio = orbitProgression / exhaustProgression;
             final double leftRatio = 1 - exhaustRatio;
